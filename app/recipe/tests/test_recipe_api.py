4b0acbe20bb99517950651db63de87ab7fa52d8a
serge <lemouton@ircam.Fr>
2021-03-27 18:06:57 +0100

    add recipe detail view

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -85,3 +85,58 @@ class PrivatRecipeApiTest(TestCase):
 		serializer= RecipeDetailSerializer(recipe)
 
 		self.assertEqual(res.data, serializer.data)
+
+	def test_create_recipe(self):
+		"""test creating basic recipe"""
+		payload = {
+		'title': 'aligot',
+		'time_minutes':30,
+		'price': 5.00}
+		res = self.client.post(RECIPES_URL,payload)
+		self.assertEqual(res.status_code,status.HTTP_201_CREATED)
+		recipe = Recipe.objects.get(id=res.data['id'])
+		for key in payload.keys():
+			self.assertEqual(payload.key,getattr(recipe,key))
+
+	def test_create_recipe_with_tags(self):
+		""" creation de recette avec tags"""
+		tag1 = sample_tag(user=self.user, 'name'='vegan')
+		tag2 = sample_tag(user=self.user, 'name'='dessert')
+		payload = {
+			'title': 'pot au feu',
+			'tags':[tag1.id, tag2.id],
+			time_minutes: 60,
+			'price': 100
+			}
+		res = self.client.post(RECIPES_URL, payload)
+
+		self.assertEqual(res.status_code,status.HTTP_201_CREATED)
+		recipe = Recipe.objects.get(id=res.data['id'])
+		tags = recipe.tags.all()
+		self.assertEqual(tags.count(),2)
+		self.assertIn(tag1,tags)
+		self.assertIn(tag2,tags)
+
+
+def test_create_recipe_with_ingredients(self):
+
+			""" creation de recette avec ingredients"""
+		ing1 = sample_ingredient(user=self.user, 'name'='poire')
+		ing2 = sample_ingredient(user=self.user, 'name'='chou')
+		payload = {
+			'title': 'pot au feu',
+			'ingredients':[ing1.id, ing2.id],
+			time_minutes: 60,
+			'price': 100
+			}
+		res = self.client.post(RECIPES_URL, payload)
+
+		self.assertEqual(res.status_code,status.HTTP_201_CREATED)
+		recipe = Recipe.objects.get(id=res.data['id'])
+		ingredients = recipe.ingredients.all()
+		self.assertEqual(ingredients.count(),2)
+		self.assertIn(ing1,ingredients)
+		self.assertIn(ing2,ingredients)
+
+	
+		
